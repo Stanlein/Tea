d63801e9174eb563ec7535205d4cecf3e2630635
Stanlein <51849778+Stanlein@users.noreply.github.com>
2021-04-23 14:35:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('app')
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/Tea/sw.js', {scope:'/Tea/'})
     .then(reg => console.log('sw is sucsesfull registzerd', reg))
     .catch( err => console.log('register sw has failed', err))
 }
